94da997ebae5ea139f3f98a7a828395cab0888cf
A S M Nasim Khan <a.s.m.nasim.khan@g.bracu.ac.bd>
2024-08-29 23:56:55 +0600

    Class 2

--- class_1.py
+++ class_1.py
@@ -1,1 +1,4 @@
 print("Eshikhon")
+print("Online")
+
+      
---
+++ class_2.py
@@ -0,0 +1,15 @@
+my_number = 6
+print(my_number)
+
+MyNumber = 7
+print(MyNumber)
+
+myNumber = 8
+print(MyNumber)
+
+myNumber_2 = 0
+print(myNumber_2)
+
+
+# A S M NASIM KHAN
+
