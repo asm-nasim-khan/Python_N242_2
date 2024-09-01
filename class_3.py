3e8c4021ae8564f211ced1e25190daff86cb31b3
A S M Nasim Khan <a.s.m.nasim.khan@g.bracu.ac.bd>
2024-09-02 00:25:25 +0600

    Class 3

---
+++ class_3.py
@@ -0,0 +1,61 @@
+# MyVariable = "Good" #String
+# YourVAriable = 5 #Integer
+# Sum = MyVariable +" "+ YourVAriable
+# print(Sum)
+
+# print(id(MyVariable))
+# print(id(YourVAriable))
+
+# num = 5
+# if num%2 == 0:
+#     print("Even")
+# else:
+#     print("odd")
+
+# name = input("Enter Your Name: ") # Input
+# # print("Hello",name) #Output
+# # print(f'Hello {name}') #Output
+# print(type(name))
+
+# print(type(MyVariable))
+# print(type(YourVAriable))
+
+# first_number = int(input("Please Enter a number: "))
+# second_number = int(input("Please Enter another number: "))
+# total = first_number + second_number
+# print(total)
+
+# Concate
+
+# #Taking two number from the user and adding them
+# first_number = input("Please Enter a number: ")
+# second_number = input("Please Enter another number: ")
+# total = int(first_number) + int(second_number)
+# print(total)
+# print(type(first_number))
+
+
+# first_number = input("Please Enter a number: ")
+# first_number = int(first_number) # Need to make it Shorter
+
+# second_number = input("Please Enter another number: ")
+# second_number = int(second_number) # Need to fix Later
+
+# total = first_number + second_number
+# print(total)
+# print(type(first_number))
+
+# first_number = int(input("Please Enter a number: "))
+
+#Single Line Comment
+
+# myName = "eshikhon"
+
+
+#Multi Line Comment
+
+"""
+This Is A multi Line Comment.
+It wont be executed by python.
+Because that how python works.
+"""
