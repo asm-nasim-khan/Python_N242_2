9447999f2369e5713473d4682183da232d30ec91
A S M Nasim Khan <a.s.m.nasim.khan@g.bracu.ac.bd>
2024-09-17 23:32:51 +0600

    Class 8 and Class 9

--- check.py
+++ check.py
@@ -23,23 +23,25 @@
 # print(msg.lower())
 
 #Assignment
-sentence_1 = "The quick brown fox jumps high."#fox
-sentence_2 = "A journey of a thousand miles."#miles
-sentence_3 = "Python coding is really fun!"#fun!
-sentence_4 = "Success comes from hard work."#work
-sentence_5 = "Always keep learning new things."#learning
-sentence_6 = "Reading books expands your mind."#expands
-sentence_7 = "Creativity requires imagination."#requires
-sentence_8 = "Consistency is key to progress."#to
-sentence_9 = "Believe in yourself and succeed."#succed
-sentence_10 = "Failure is the key to success."#to
-print(sentence_1[16:19])
-print(sentence_2[24:31])
-print(sentence_3[24:28])
-print(sentence_4[24:29])
-print(sentence_5[12:20])
-print(sentence_6[14:21])
-print(sentence_7[11:19])
-print(sentence_8[19:21])
-print(sentence_9[24:32])
-print(sentence_10[19:21])
+# sentence_1 = "The quick brown fox jumps high."#fox
+# sentence_2 = "A journey of a thousand miles."#miles
+# sentence_3 = "Python coding is really fun!"#fun!
+# sentence_4 = "Success comes from hard work."#work
+# sentence_5 = "Always keep learning new things."#learning
+# sentence_6 = "Reading books expands your mind."#expands
+# sentence_7 = "Creativity requires imagination."#requires
+# sentence_8 = "Consistency is key to progress."#to
+# sentence_9 = "Believe in yourself and succeed."#succed
+# sentence_10 = "Failure is the key to success."#to
+# print(sentence_1[16:19])
+# print(sentence_2[24:31])
+# print(sentence_3[24:28])
+# print(sentence_4[24:29])
+# print(sentence_5[12:20])
+# print(sentence_6[14:21])
+# print(sentence_7[11:19])
+# print(sentence_8[19:21])
+# print(sentence_9[24:32])
+# print(sentence_10[19:21])
+
+print(True or False and False)
---
+++ class_9.py
@@ -0,0 +1,60 @@
+# Q1. Take 3 numbers from user and find the largest number
+
+#Tuples 
+
+# lst = [2,3,4]
+
+# lst[0] = 5
+# lst[1] = 10
+
+
+# print(lst)
+
+my_tup = (98,4)
+# print(my_tup[1:])
+# my_tup[0] = 100
+
+# math, bangla = my_tup
+
+# print(math,bangla)
+# my_tup = (98,4,10)
+
+# my_tup = list(my_tup)
+# # print(type(my_tup))
+# my_tup[0] = 100
+# my_tup = tuple(my_tup)
+# print(my_tup)
+
+# msg = "10"
+# num = int(msg)
+# Typecasting
+
+# constant = (3.1416,9.8)
+# pi = constant[0]
+
+# Sets
+
+my_set = {1,2,3,3}
+# my_set.add(4)
+# print(my_set)
+
+DaSet = {"Mango","Banana","Apple"}
+# DaSet.pop()
+# DaSet.remove("Mango")
+# DaSet.discard("Mango1")
+
+# print(DaSet)
+
+# my_set.update(DaSet)
+# print(my_set)
+
+# Dictionary
+# my_dict1 = {
+#     "Mango" : 10,
+#     "Banana" : 15,
+#     "Apple" : 25
+# }
+
+# my_dict2 = {'Mango': 10, 'Banana': 15, 'Apple': 25}
+
+# print(my_dict1["Mango"])
