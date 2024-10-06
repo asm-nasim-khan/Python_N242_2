08a12ddc8822c120aadd09388868677a70b66fd9
A S M Nasim Khan <a.s.m.nasim.khan@g.bracu.ac.bd>
2024-10-07 01:12:43 +0600

    class 17

---
+++ class_17.py
@@ -0,0 +1,18 @@
+# lst = [9,5,2,4,65,7]
+
+# print this list using loop
+# print(lst)
+# Find the maximum and minimum number in the list
+
+# max_num = lst[0] # 65
+# min_num = lst[0] # 2
+
+# for x in range(len(lst)):
+#     if lst[x]>max_num:
+#         max_num = lst[x]
+#     elif lst[x]<min_num:
+#         min_num = lst[x]
+
+# print(max_num, min_num)
+
+
