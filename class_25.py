8cbfad2869b6e0b64b940c3c94af5cab98f18bbc
A S M Nasim Khan <a.s.m.nasim.khan@g.bracu.ac.bd>
2024-10-31 23:18:15 +0600

    Class 25

---
+++ class_25.py
@@ -0,0 +1,82 @@
+# def Greet(name):
+#     print("Good morning", name)
+#     print("Good Noon", name)
+#     print("Good Afternoon", name)
+#     print("Good Night", name)
+
+# Greet("Nasim")
+# Greet("Joshim")
+
+# class vehical:
+#     window = 2
+#     wheel = 4
+#     seats = 4
+#     horn_sound = "beep beep"
+#     def horn(self):
+#         print(self.horn_sound)
+
+# #Driver Code
+# Toyota = vehical()
+
+# Toyota.horn()
+# Toyota.horn_sound = "vruum Vruum"
+# Toyota.horn()
+
+# print(Toyota.window)
+# print(Toyota.seats)
+# print(Toyota.wheel)
+# print(Toyota.horn)
+
+# truck = vehical()
+# truck.horn()
+
+# class Bird:
+#     name = "Bird"
+#     def print_name(self):
+#         print(self.name)
+    
+#     def flight(self):
+#         print(self.name,"is Flying.")
+
+# class Cuckoo(Bird):
+
+#     def flight(self):
+#         print(self.name,"is Flying happily.")
+
+# Cuckoo_1 = Cuckoo()
+# Cuckoo_1.flight()
+# Cuckoo_1.print_name()
+
+
+
+
+# cuckoo = Bird()
+# cuckoo.name = "Cuckoo"
+# cuckoo.flight()
+
+# Moyna = Bird()
+# Moyna.flight()
+
+
+class Eshikhon_Bank:
+    name = "User"
+    balance = 0.0
+    def deposite(self,amount):
+        self.balance  += amount
+        print(amount,"Has been disposited")
+    
+    def withdraw(self,amount):
+        print(amount,"has been Withdrawn")
+
+user_1 = Eshikhon_Bank()
+user_1.name = "Nasim"
+user_1.deposite(10000)
+
+print(user_1.balance)
+user_2 = Eshikhon_Bank()
+print(user_2.balance)
+user_3 = Eshikhon_Bank()
+print(user_3.balance)
+
+user_4 = Eshikhon_Bank()
+user_5 = Eshikhon_Bank()
